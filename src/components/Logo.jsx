de29e34dcd2b794589fc17831c73f25c8062af09
bhavya dalal <bhavyadalal@192.168.1.8>
2024-02-10 13:39:00 +0530

    first commit

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 function Logo() {
   return (
     <div className='mainLogo items-center'>
-      <img className="logo" src="./src/components/list.png" alt="" />
+      <img className="logo" src="/list.png" alt="" />
       <span className='logoName ml-3 text-xl font-serif font-bold'>TaskWave</span>
     </div>
   )
